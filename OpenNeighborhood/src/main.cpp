9ab461300df1bae9fdf57be8b43fe4803ecc1078
Clement <cdreptin@gmail.com>
2020-12-23 01:33:20 +0100

    Removed all XBDM related code for now

--- OpenNeighborhood/src/main.cpp
+++ OpenNeighborhood/src/main.cpp
@@ -1,28 +1,6 @@
 #include "pch.h"
 
-#include <Xbdm.h>
-
-using namespace XBDM;
-
 int main()
 {
-	DevConsole console("192.168.1.104");
-	
-	if (!console.OpenConnection())
-	{
-		LOG_ERROR("Couldn't connect to console!");
-		return 0;
-	}
-
-	LOG_SUCCESS("Connected to console successfully!");
-
-	if (!console.CloseConnection())
-	{
-		LOG_ERROR("Couldn't disconnect from console!");
-		return 0;
-	}
-
-	LOG_SUCCESS("Disconnected from console successfully!");
-
 	return 0;
 }
