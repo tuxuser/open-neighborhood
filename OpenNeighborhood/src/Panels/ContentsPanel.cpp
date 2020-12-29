8df0af60fa492cb1f97de2c61e03efc8913249c5
Clement <cdreptin@gmail.com>
2020-12-29 09:22:26 +0100

    Added an "Add Xbox 360" button (not functional yet)

--- OpenNeighborhood/src/Panels/ContentsPanel.cpp
+++ OpenNeighborhood/src/Panels/ContentsPanel.cpp
@@ -1,6 +1,12 @@
 #include "pch.h"
 #include "Panels/ContentsPanel.h"
 
+ContentsPanel::ContentsPanel()
+{
+	m_Texture = std::make_shared<Texture>("assets/icons/addXboxButton.png");
+	m_Texture->Bind();
+}
+
 void ContentsPanel::OnRender()
 {
 	float pathPanelSize = m_Margin * 5.0f;
@@ -18,5 +24,6 @@ void ContentsPanel::OnRender()
 	ImGui::SetNextWindowSize(ImVec2((float)m_WindowWidth - m_Margin * 2.0f, (float)m_WindowHeight - (m_Margin * 3.0f + pathPanelSize)));
 
 	ImGui::Begin("Contents Window", nullptr, windowFlags);
+	ImGui::ImageButtonWithText((void*)(intptr_t)m_Texture->GetTexture(), ImVec2(100.0f, 100.0f), "Add Xbox 360");
 	ImGui::End();
 }
--- OpenNeighborhood/src/Panels/ContentsPanel.h
+++ OpenNeighborhood/src/Panels/ContentsPanel.h
@@ -2,9 +2,14 @@
 
 #include "Panels/Panel.h"
 #include "Events/AppEvent.h"
+#include "Render/Texture.h"
 
 class ContentsPanel : public Panel
 {
 public:
+	ContentsPanel();
+
 	virtual void OnRender() override;
+private:
+	Ref<Texture> m_Texture;
 };
